2e6942f95e9989ca4f11ccc8559f398f988065e2
nicolaspozzo <nicolaspozzo19@gmail.com>
2019-08-10 19:38:19 -0300

    avances en el login y capaDao
    Cree la clase ConnectionString que tiene la cadena de coneccion a mi base de datos, cada uno debe poner su cadena de coneccion y comentar el resto para poder acceder a la bd
    Cree la clase usuarioDao que busca los permisos y el usuario para el login, me falta terminar las sentencias sql

---
+++ HayEquipo/CapaDao/UsuarioDao.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CapaDao
+{
+   public class UsuarioDao
+    {
+
+        public static bool Usuario(string usuario, string clave) {
+
+            bool flag = false;
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = ConnectionString.Cadena();
+            cn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = @"SELECT * FROM Usuario
+                                WHERE nombre = @usuario ADN contraseña = @clave";
+            cmd.Parameters.AddWithValue("@usuario", usuario);
+            cmd.Parameters.AddWithValue("@clave",clave);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                flag = true;
+            }
+            dr.Close();
+            cn.Close();
+            return flag;
+            
+        }
+
+        public static string Permiso(string usuario)
+        {
+            string permiso = string.Empty;
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = ConnectionString.Cadena();
+            cn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = @"SELECT r.nombre as nombrePermiso
+                                FROM Usuario u, Rol r
+                                WHERE u.id";
+
+
+            return permiso;
+        }
+
+
+    }
+}
--- HayEquipo/CapaPresentacion/Login.aspx.cs
+++ HayEquipo/CapaPresentacion/Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CapaDao;
 
 namespace CapaPresentacion
 {
@@ -16,7 +17,25 @@ namespace CapaPresentacion
 
         protected void btn_Login_Click(object sender, EventArgs e)
         {
-            Response.Redirect("Home.aspx");
+            if (validarUsuario(txt_NombreUsuario.Text, txt_Password.Text)){
+
+                Session["Usuario"] = txt_NombreUsuario.Text;
+                Response.Redirect("Home.aspx");
+            }
+           
+        }
+
+        public bool validarUsuario(string usuario, string clave) {
+
+            bool flag = false;
+            if (UsuarioDao.Usuario(usuario, clave))
+            {
+                flag = true;
+                Session["Rol"] = UsuarioDao.Permiso(usuario);
+            }
+
+            return flag;
+           
         }
     }
 }
