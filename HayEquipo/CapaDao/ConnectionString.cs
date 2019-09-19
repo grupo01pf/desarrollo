8745f0e9f8ea4e5546dca6dfd1880e1bae682cf4
elotronacho <53498193+elotronacho@users.noreply.github.com>
2019-09-19 01:05:46 -0300

    Configurando Entity en PC Nacho

--- HayEquipo/CapaDao/ConnectionString.cs
+++ HayEquipo/CapaDao/ConnectionString.cs
@@ -41,8 +41,9 @@ namespace CapaDao
             //FrancoR BD
             // String cadena = "Data Source=LAPTOP-N4V2H3NE;Initial Catalog=HayEquipo;User ID=alumnosa;Password=abcd123";
 
-            string cadena = @"Data Source=DESKTOP-A9A7OVR\SQLEXPRESS;Initial Catalog=HayEquipo;User ID=SA;Password=taninga1643";
-
+            //string cadena = @"Data Source=DESKTOP-A9A7OVR\SQLEXPRESS;Initial Catalog=HayEquipo;User ID=SA;Password=taninga1643";
+            //Otra cadena de Nacho:
+            string cadena = @"Data Source=IGNACIOMONETTOP\SQLEXPRESS;Initial Catalog=HayEquipo;Integrated Security=True";
 
             //return cadeana;
 
@@ -69,7 +70,9 @@ namespace CapaDao
             //conexion.ConnectionString = "Data Source=DESKTOP-H7ESUE9;Initial Catalog=HayEquipo;Integrated Security=True";
 
             //Cadena de Conexión de Nacho.
-            conexion.ConnectionString = @"Data Source=DESKTOP-A9A7OVR\SQLEXPRESS;Initial Catalog=HayEquipo;Persist Security Info=True;User ID=SA;Password=taninga1643";
+            //conexion.ConnectionString = @"Data Source=DESKTOP-A9A7OVR\SQLEXPRESS;Initial Catalog=HayEquipo;Persist Security Info=True;User ID=SA;Password=taninga1643";
+            //Otra cadena de conexion de Nacho:
+            conexion.ConnectionString = @"Data Source=IGNACIOMONETTOP\SQLEXPRESS;Initial Catalog=HayEquipo;Integrated Security=True";
 
             //Cadena de conexion de Agus
             //conexion.ConnectionString = @"Data Source=DESKTOP-23N56UG\SQLEXPRESS;Initial Catalog=HayEquipo;Integrated Security=True";
