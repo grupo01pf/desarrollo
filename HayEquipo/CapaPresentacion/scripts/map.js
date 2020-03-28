27196d32fc05038af489b2e963427eada29c3a3d
nicolaspozzo <nicolaspozzo19@gmail.com>
2020-03-28 16:19:20 -0300

    latitude and longitude ok

--- HayEquipo/CapaPresentacion/scripts/map.js
+++ HayEquipo/CapaPresentacion/scripts/map.js
@@ -2,19 +2,65 @@
 
 const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png '
 
+
+// centrar la vista del mapa
 let myMap = L.map('myMap').setView([-31.416563, -64.183533], 12)
 
 L.tileLayer(tilesProvider, {
     maxzoom: 18,
 }).addTo(myMap)
 
+// crear un marcador
+// let marker = L.marker([-31.416563, -64.183533]).addTo(myMap)
 
-let marker = L.marker([-31.416563, -64.183533]).addTo(myMap)
 
 myMap.doubleClickZoom.disable()
+
+
+var layerGroup = L.layerGroup().addTo(myMap)
+
+
+// poner un marcador al hacer dobleclick
 myMap.on('dblclick', e => {
+
+    layerGroup.clearLayers();
+
+
     let latLng = myMap.mouseEventToLatLng(e.originalEvent)
-    L.marker([latLng.lat, latLng.lng]).addTo(myMap)
+   // L.marker([latLng.lat, latLng.lng]).addTo(myMap)
+
+    myMap.closePopup();
+    var marker = L.marker([latLng.lat, latLng.lng]).addTo(layerGroup)
+
+   // $('#txt_Latitud').val(myMap.getCenter().lat + ',' + myMap.getCenter().lng); //ok
+
+
+    $('#txt_Latitud').val(marker.getLatLng().lat)
+    $('#txt_Longitud').val(marker.getLatLng().lng)
+
+    // marker.bindPopup('CBA').openPopup(); // ok
+
+  
+    
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
